f38b4f2f93be92c4648b4c851e4377f44b764b39
moonglum <moonglum@moonbeamlabs.com>
2013-10-10 09:38:47 +0200

    Initial Commit: Rails Generate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AshikawaBlog::Application.config.secret_key_base = 'b085c7d46ca31d115d76e1429353d0f8ae686142660ba5c017ccb944e40e870ba5872c0fbce1bca8467ef391bef28ae2b70f63a4ed888bc513e0bd5eb49a9b63'
