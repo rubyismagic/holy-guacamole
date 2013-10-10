0d0ae5473743797501ef152f6e35b410f5fe8bbd
moonglum <moonglum@moonbeamlabs.com>
2013-10-10 11:19:43 +0200

    Extracted Ashikawa::Rails::Model from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
+require 'active_support/concern'
+
+module Ashikawa
+  module Rails
+    module Model
+      extend ActiveSupport::Concern
+
+      included do
+        include ActiveModel::Validations
+        include ActiveModel::Naming
+        include Virtus.model
+      end
+    end
+  end
+end
+
 class User
-  include ActiveModel::Validations
-  include ActiveModel::Naming
-  include Virtus.model
+  include Ashikawa::Rails::Model
 
   attribute :name, String
   attribute :email, String
